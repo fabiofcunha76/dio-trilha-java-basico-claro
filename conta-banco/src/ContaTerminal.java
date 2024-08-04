85713bd397e9229321224bda4d1e8ae035df4f0c
Fabio F Cunha <fabiofcunha76@gmail.com>
2024-08-03 22:00:53 -0300

    exercício de java conta bancária DIO.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,33 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
-        System.out.println("Hello, World!");
+
+        int numeroConta = 0;
+        String agencia;
+        String nomeCliente;
+        BigDecimal saldo;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor informe seu nome: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor informe a sua agência:  ");
+        agencia = scanner.nextLine();
+
+        System.out.print("Favor informe o número da sua conta bancária: ");
+        numeroConta = scanner.nextInt();
+
+        System.out.print("Por favor informe o saldo da sua conta: ");
+        saldo = scanner.nextBigDecimal();
+
+        System.out.println();
+
+        System.out.println("Olá " + nomeCliente + "!"
+                + " Obrigado por criar uma conta em nosso banco, sua agência é " +
+                agencia + ", conta " + numeroConta + " e seu saldo " + saldo.toString()
+                + " já está disponível para saque.");
     }
 }
